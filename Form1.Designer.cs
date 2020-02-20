c011d22f806e8efcf0222b628300a2a9cb5705a4
himo77 <56316743+himo77@users.noreply.github.com>
2020-02-20 11:09:27 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Weight_on_Mars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWeightEarth = new System.Windows.Forms.TextBox();
+            this.txtWeightMars = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtObject = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weight on Earth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Weight on Mars";
+            // 
+            // txtWeightEarth
+            // 
+            this.txtWeightEarth.Location = new System.Drawing.Point(465, 122);
+            this.txtWeightEarth.Name = "txtWeightEarth";
+            this.txtWeightEarth.Size = new System.Drawing.Size(273, 31);
+            this.txtWeightEarth.TabIndex = 2;
+            // 
+            // txtWeightMars
+            // 
+            this.txtWeightMars.Location = new System.Drawing.Point(462, 206);
+            this.txtWeightMars.Name = "txtWeightMars";
+            this.txtWeightMars.ReadOnly = true;
+            this.txtWeightMars.Size = new System.Drawing.Size(276, 31);
+            this.txtWeightMars.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(175, 285);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(240, 85);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtObject
+            // 
+            this.txtObject.Location = new System.Drawing.Point(465, 27);
+            this.txtObject.MaxLength = 50;
+            this.txtObject.Name = "txtObject";
+            this.txtObject.Size = new System.Drawing.Size(273, 31);
+            this.txtObject.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Name of the object";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, -52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(805, 500);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtObject);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWeightMars);
+            this.Controls.Add(this.txtWeightEarth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Mars Weight";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWeightEarth;
+        private System.Windows.Forms.TextBox txtWeightMars;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtObject;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weight_on_Mars
+{
+    public partial class Form1 : Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (!ValidateName(txtObject.Text, out string objectName, out string nameErrorMessage))
+            {
+
+                MessageBox.Show(nameErrorMessage, "Object Name Error");
+                txtObject.Focus(); // Move focus to this TextBox for user to correct error
+                return;  // stop processing this event
+            }
+
+            if (!ValidatePositiveDouble(txtWeightEarth.Text, out double earthWeight, out string weightErrorMessage))
+            {
+                MessageBox.Show(weightErrorMessage, "Earth Weight Error");
+                txtWeightEarth.Focus();  // Move focus to this TextBox for user to correct error
+                return;  // stop processing this event
+
+            }
+            double conversionFactor = 0.377;
+            double marsWeight = earthWeight * conversionFactor;
+            txtWeightMars.Text = String.Format("{0} weighs {1} on Mars", objectName, marsWeight);
+        }
+
+        private bool ValidateName(string text, out string name, out string errorMessage)
+        {
+            errorMessage = null;
+            name = text;
+
+            if (String.IsNullOrEmpty(text))
+            {
+                errorMessage = "Cant be empty";
+                return false;
+            }
+
+            if (text.Length < 2)
+            {
+                errorMessage = "Enter at least 2 letter";
+                return false;
+            }
+            return true;
+        }
+
+
+        private bool ValidatePositiveDouble(string text, out double number, out string errorMessage)
+        {
+
+            errorMessage = null;
+            number = 0;
+
+            try
+            {
+                number = double.Parse(text);
+
+                if (number >= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    errorMessage = "Enter a positive number";
+                    return false;
+                }
+            }
+            catch (FormatException)
+            {
+                errorMessage = "Enter a number";
+                return false;
+            }
+            catch (OverflowException)
+            {
+                errorMessage = "Enter a smaller number";
+            }
+            return false;
+        }
+    }
+}
+
+        
+
+   
+
+
+
